4362a408d238a8123584518e1947bc0ede77aa88
sanns <iamthe@bk.ru>
2018-02-16 16:12:48 +0300

    Added case for less than 50 notes selected. You can see now if every of these notes has a tag in a dialog window. copypasted reformatting/extracted function/renaming

--- omniNotes/src/main/java/it/feio/android/omninotes/utils/TagsHelper.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/utils/TagsHelper.java
@@ -27,6 +27,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 
 
@@ -38,10 +39,22 @@ public class TagsHelper {
     }
 
 
+    /**
+     * Returns a map of space-separated text tokens (matched to hashtag pattern) to count of how many theirs found.
+     *
+     *
+     * Attention: now is resource-intensive algorithm to search whole note for hashtag text tokens.
+     * */
+    // todo reduce computational complexity.
 	public static HashMap<String, Integer> retrieveTags(Note note) {
 		HashMap<String, Integer> tagsMap = new HashMap<>();
-		for (String token : (note.getTitle() + " " + note.getContent()).replaceAll("\n", " ").trim().split(" ")) {
+
+        String[] wordsOfTheNote = (note.getTitle() + " " + note.getContent())
+          .replaceAll("\n", " ").trim().split(" ");
+
+        for (String token : wordsOfTheNote) {
 			if (RegexPatternsConstants.HASH_TAG.matcher(token).matches()) {
+                //increment count of found tokens in a hashmap.
 				int count = tagsMap.get(token) == null ? 0 : tagsMap.get(token);
 				tagsMap.put(token, ++count);
 			}
@@ -109,20 +122,56 @@ public class TagsHelper {
         return getPreselectedTagsArray(notes, tags);
     }
 
+    public static Integer getIndexInList(String tagtext, List<Tag> tags) {
+        for (Tag tag : tags) {
+            if (tag.getText().equals(tagtext)) {
+                return tags.indexOf(tag);
+            }
+        }
+        return null;
+    }
 
+    /**
+     * Returns empty Integer[] if more than one Note in notes. If one note then indices of every tag in {tags} List  that match this one note.
+     * If less than 50, then returns indices of {tags} which included in every of these notes.
+     * @param notes some notes
+     * @param tags All tags for app.
+     * */
     public static Integer[] getPreselectedTagsArray(List<Note> notes, List<Tag> tags) {
         final Integer[] preSelectedTags;
         if (notes.size() == 1) {
-            List<Integer> t = new ArrayList<>();
+            List<Integer> unknownSizeArray = new ArrayList<>();
             for (String noteTag : TagsHelper.retrieveTags(notes.get(0)).keySet()) {
-                for (Tag tag : tags) {
-                    if (tag.getText().equals(noteTag)) {
-                        t.add(tags.indexOf(tag));
-                        break;
+                unknownSizeArray.add(getIndexInList(noteTag, tags));
                     }
+            //Cannot generate this array in a first place, because we don't know the exact size.
+            preSelectedTags = unknownSizeArray.toArray(new Integer[unknownSizeArray.size()]);
+        } else if (notes.size() <= 50){
+            //go through all notes
+            //search for each tag if it is in Note, or for tags in Note if it in all other Notes.
+            HashMap<String, Integer> hashtagToNotesQuantity = new HashMap<>();
+
+            for (Note note: notes){
+                Set<String> hashtags = TagsHelper.retrieveTags(note).keySet();
+                for(String hashtag : hashtags){
+                    final Integer quantity = hashtagToNotesQuantity.get(hashtag);
+                    if (quantity == null) hashtagToNotesQuantity.put(hashtag, 1);
+                    else hashtagToNotesQuantity.put(hashtag, quantity+1);
                 }
             }
-            preSelectedTags = t.toArray(new Integer[t.size()]);
+
+            List<Integer> unknownSizeArray = new ArrayList<>();
+
+            for (String hashtag : hashtagToNotesQuantity.keySet()) {
+                Integer quantity = hashtagToNotesQuantity.get(hashtag);
+                if( quantity != null && quantity == notes.size()) {
+                    //this hashtag is in every note.
+                    unknownSizeArray.add(getIndexInList(hashtag, tags));
+                }
+            }
+
+            //Cannot generate this array in a first place, because we don't know the exact size.
+            preSelectedTags = unknownSizeArray.toArray(new Integer[unknownSizeArray.size()]);
         } else {
             preSelectedTags = new Integer[]{};
         }
